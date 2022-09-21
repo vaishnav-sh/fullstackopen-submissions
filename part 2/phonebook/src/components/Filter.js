ce1aab532fdf6afd0f103f4e5c6a346b11c05955
Vaishnav <vaishnav.s@talview.com>
2022-09-21 15:14:24 +0530

    Revert "part 2 exercises  completed"
    This reverts commit 940949f5e2c3b7efa8a56f9c5a5ca298879c2268.

--- part 2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Filter = ({ value, onChange, text }) => {
-    return (<p>{text}
-        <input value={value} onChange={onChange} />
-    </p>)
-}
-
-export default Filter
